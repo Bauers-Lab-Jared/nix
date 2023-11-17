c7e7a55a320afd478140e2135d765c6cf19c56d6
Jared <127258074+Bauers-Lab-Jared@users.noreply.github.com>
2023-11-16 21:49:35 -0500

    migrating to Snowfall flake framework

--- flake.nix
+++ flake.nix
@@ -1,55 +1,97 @@
 #https://nixos-and-flakes.thiscute.world/introduction/
 #https://jdisaacs.com/blog/nixos-config/
+#https://snowfall.org/guides/lib/quickstart/
 rec {
   description = "Bauer's Lab Flake";
 
   inputs = {
-    # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    # Home manager
+    #secrets management
+    #?   agenix.url = "github:ryantm/agenix";
+    #modularizes deployment
+    #?   deploy-rs.url = "github:serokell/deploy-rs";
+    #Utilities for opt-in persistance
+    impermanence.url = "github:riscadoa/impermanence";
+    #adapts the nixos module system for config on individual users
     home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    #the SOURCE
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    #hardware config library
+    hardware.url = "github:nixos/nixos-hardware";
+    #utility functions for building flakes
+    utils.url = "github:numtide/flake-utils";
 
-    # Utilities for building our flake
-    flake-utils.url = "github:numtide/flake-utils";
+    #provides a flake framework
+    snowfall-lib = {
+        url = "github:snowfallorg/lib";
+        inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-    util.url = "path:./flake/util";
-    util.inputs.nixpkgs.follows = "nixpkgs";
-    # Extra flakes for modules, packages, etc
-    hardware.url = "github:nixos/nixos-hardware"; # Convenience modules for hardware-specific quirks
-    # nur.url = "github:nix-community/NUR"; # User contributed pkgs and modules
-    # nix-colors.url = "github:misterio77/nix-colors"; # Color schemes for usage with home-manager
-    # impermanence.url = "github:riscadoa/impermanence"; # Utilities for opt-in persistance
-    # agenix.url = "github:ryantm/agenix"; # Secrets management
+    #simplified command line calls for flakes
+    snowfall-flake = {
+			url = "github:snowfallorg/flake";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
 
-     # Window manager
-    # hyprland = {
-    #   url = "github:hyprwm/Hyprland";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
-    # hyprland-contrib = {
-    #   url = "github:hyprwm/contrib";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
+    #It's for a friend, I swear...
+    vscode-server = {
+      url = "github:nix-community/nixos-vscode-server";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "utils";
+    };
 
+    #When life gives you windows...
+    wsl = {
+      url = "github:nix-community/NixOS-WSL";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "utils";
+    };
+
+    #Do you even vim, bruv?
     nixvim = {
       url = "github:nix-community/nixvim";
-      # If you are not running an unstable channel of nixpkgs, select the corresponding branch of nixvim.
-      # url = "github:nix-community/nixvim/nixos-23.05";
-
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    nixos-wsl = {
-      url = "github:nix-community/nixos-wsl";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    
+    #util.url = "path:./flake/util";
+    #util.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = {self, ... } @ inputs: let 
-    inherit (self) outputs;
-  in 
-  ( import ./flake {inherit inputs outputs;} );
-  
+  outputs = inputs: 
+    inputs.snowfall-lib.mkFlake {
+      # You must provide our flake inputs to Snowfall Lib.
+      inherit inputs;
+
+      # The `src` must be the root of the flake. See configuration
+      # in the next section for information on how you can move your
+      # Nix files to a separate directory.
+      src = ./.;
+
+      # Configure Snowfall Lib, all of these settings are optional.
+      snowfall = {
+          # Tell Snowfall Lib to look in the `./nix/` directory for your
+          # Nix files.
+          root = ./nix;
+
+          # Choose a namespace to use for your flake's packages, library,
+          # and overlays.
+          namespace = "thisFlake";
+
+          # Add flake metadata that can be processed by tools like Snowfall Frost.
+          meta = {
+              # A slug to use in documentation when displaying things like file paths.
+              name = "bauers-lab-flake";
+
+              # A title to show for your flake, typically the name.
+              title = "bauers-lab-flake";
+          };
+      };
+
+      # The outputs builder receives an attribute set of your available NixPkgs channels.
+      # These are every input that points to a NixPkgs instance (even forks). In this
+      outputs-builder = channels: {
+          # Outputs in the outputs builder are transformed to support each system. This
+          # entry will be turned into multiple different outputs like `formatter.x86_64-linux.*`.
+          # EX: formatter = channels.nixpkgs.alejandra;
+      };
+    };
 }
