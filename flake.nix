39c1f1474d4823cbed8f03843f4ead2e489306f3
Jared <127258074+Bauers-Lab-Jared@users.noreply.github.com>
2024-02-05 09:30:26 -0500

    switched back to bauers lab nvim flake

--- flake.nix
+++ flake.nix
@@ -38,7 +38,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    neovim-flake.url = "github:jordanisaacs/neovim-flake";
+    neovim-flake.url = "github:bauers-lab-jared/neovim-flake";
 
     #It's for a friend, I swear...
     vscode-server = {
