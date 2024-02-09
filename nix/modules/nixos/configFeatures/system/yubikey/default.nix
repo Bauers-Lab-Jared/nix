32d32a73a8a8ebd6dc2ca5673c418371813e21ec
Jared <127258074+Bauers-Lab-Jared@users.noreply.github.com>
2024-02-09 10:14:18 -0500

    yubikey

--- nix/modules/nixos/configFeatures/system/yubikey/default.nix
+++ nix/modules/nixos/configFeatures/system/yubikey/default.nix
@@ -48,13 +48,11 @@ in {
       services.udev.packages = [ pkgs.yubikey-personalization ];
     }
     (mkIf config.thisFlake.configFeatures.wsl.enable {
-      udev = {
-        enable = true;
-        packages = [pkgs.yubikey-personalization];
-        #extraRules = ''
-        #  SUBSYSTEM=="usb", MODE="0666"
-        #  KERNEL=="hidraw*", SUBSYSTEM=="hidraw", TAG+="uaccess", MODE="0666"
-        #'';
+      services.udev = {
+        extraRules = ''
+          SUBSYSTEM=="usb", MODE="0666"
+          KERNEL=="hidraw*", SUBSYSTEM=="hidraw", TAG+="uaccess", MODE="0666"
+        '';
       };
     })
   ]);
