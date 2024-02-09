a3d9787e62607c8a15c8834506049a37c2f3a5f4
Jared <127258074+Bauers-Lab-Jared@users.noreply.github.com>
2024-02-09 12:21:54 -0500

    cleaning libs

--- lib/features/default.nix
+++ lib/features/default.nix
@@ -9,8 +9,67 @@
     # Additionally, Snowfall Lib's own inputs are passed. You probably don't need to use this!
     snowfall-inputs,
 }: with lib;
-with lib.thisFlake;
+with lib.thisFlake.;
 rec {
+
+     ## Create a NixOS module option.
+  ##
+  ## ```nix
+  ## lib.mkOpt nixpkgs.lib.types.str "My default" "Description of my option."
+  ## ```
+  ##
+  #@ Type -> Any -> String
+  mkOpt = type: default: description:
+    mkOption { inherit type default description; };
+
+  ## Create a NixOS module option without a description.
+  ##
+  ## ```nix
+  ## lib.mkOpt' nixpkgs.lib.types.str "My default"
+  ## ```
+  ##
+  #@ Type -> Any -> String
+  mkOpt' = type: default: mkOpt type default null;
+
+  ## Create a boolean NixOS module option.
+  ##
+  ## ```nix
+  ## lib.mkBoolOpt true "Description of my option."
+  ## ```
+  ##
+  #@ Type -> Any -> String
+  mkBoolOpt = mkOpt types.bool;
+
+  ## Create a boolean NixOS module option without a description.
+  ##
+  ## ```nix
+  ## lib.mkBoolOpt true
+  ## ```
+  ##
+  #@ Type -> Any -> String
+  mkBoolOpt' = mkOpt' types.bool;
+
+  enabled = {
+    ## Quickly enable an option.
+    ##
+    ## ```nix
+    ## services.nginx = enabled;
+    ## ```
+    ##
+    #@ true
+    enable = true;
+  };
+
+  disabled = {
+    ## Quickly disable an option.
+    ##
+    ## ```nix
+    ## services.nginx = enabled;
+    ## ```
+    ##
+    #@ false
+    enable = false;
+  };
 
     mkConfigFeature = {config, featureName, otherOptions}: (recursiveUpdate 
         {thisFlake.configFeatures.${featureName}.enable = 
