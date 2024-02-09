127cff6f9f85f516a79e5d22a8b7bec9fa67ce62
Jared <127258074+Bauers-Lab-Jared@users.noreply.github.com>
2024-02-09 12:01:58 -0500

    cleaning libs

--- lib/features/default.nix
+++ lib/features/default.nix
@@ -9,14 +9,7 @@
     # Additionally, Snowfall Lib's own inputs are passed. You probably don't need to use this!
     snowfall-inputs,
 }: with lib; 
-let
-    mkOpt = type: default: description:
-        mkOption { inherit type default description; };
-
-    mkOpt' = type: default: mkOpt type default null;
-
-    mkBoolOpt = mkOpt types.bool;
-in rec {
+rec {
 
     mkConfigFeature = {config, featureName, otherOptions}: (recursiveUpdate 
         {thisFlake.configFeatures.${featureName}.enable = 
