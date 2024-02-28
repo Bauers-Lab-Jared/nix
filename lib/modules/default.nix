c9581da73e460cced7e92ea353a4c0d3cbeccdcd
Jared <127258074+Bauers-Lab-Jared@users.noreply.github.com>
2024-02-27 23:32:13 -0500

    added mkDefault' func for layering default values

--- lib/modules/default.nix
+++ lib/modules/default.nix
@@ -4,4 +4,7 @@ with builtins; rec {
   
   mkDefaultEach = atterSet: mapAttrs (name: value: mkDefault value) atterSet;
 
+  mkDefault' = level: v: mkOverride (1000 - (level or 0)) v;
+  mkDefaultEach' = level: atterSet: mapAttrs (name: value: mkDefault' level value) atterSet;
+
 }
