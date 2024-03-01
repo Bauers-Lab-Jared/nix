27182d2e3ea1d9ac0e6b10225b9b79129dacc140
Jared <127258074+Bauers-Lab-Jared@users.noreply.github.com>
2024-02-29 21:21:44 -0500

    removing  unnecessary persist dirs

--- nix/modules/nixos/systemFeatures/features/impermanence/impermanence.nix
+++ nix/modules/nixos/systemFeatures/features/impermanence/impermanence.nix
@@ -40,11 +40,6 @@ let
         "/var/log"
         "/var/lib/nixos"
         "/var/lib/systemd/coredump"
-        { directory = "/var/lib/colord"; user = "colord"; group = "colord"; mode = "u=rwx,g=rx,o="; }
-      ];
-      files = [
-        "/etc/machine-id"
-        { file = "/var/keys/secret_file"; parentDirectory = { mode = "u=rwx,g=,o="; }; }
       ];
     };
   };
