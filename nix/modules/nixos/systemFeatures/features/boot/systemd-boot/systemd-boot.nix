372ec598c19a67ae053fa246a630fdb2a93610c3
Jared <127258074+Bauers-Lab-Jared@users.noreply.github.com>
2024-03-09 23:28:36 -0500

    debug boot on any fail

--- nix/modules/nixos/systemFeatures/features/boot/systemd-boot/systemd-boot.nix
+++ nix/modules/nixos/systemFeatures/features/boot/systemd-boot/systemd-boot.nix
@@ -41,6 +41,7 @@ let
       "rd.udev.log_level=3"
       "udev.log_priority=3"
       "boot.shell_on_fail"
+      "boot.debug1"
     ];
     # https://github.com/NixOS/nixpkgs/blob/c32c39d6f3b1fe6514598fa40ad2cf9ce22c3fb7/nixos/modules/system/boot/loader/systemd-boot/systemd-boot.nix#L66
     boot.loader.systemd-boot.editor = false;
